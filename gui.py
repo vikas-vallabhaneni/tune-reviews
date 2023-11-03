b54f36f022c7243dc29d506e1fc9c0609be6eee7
markush1022 <homannm@vt.edu>
2023-11-03 17:09:34 -0400

    working

--- gui.py
+++ gui.py
@@ -6,6 +6,8 @@ load_dotenv()
 
 import os
 
+
+
 # Create a function to handle inserting data into the selected MySQL table
 def insert_data(table_name):
     if table_name not in ("User", "Artist", "Album", "Song", "Review"):
@@ -23,7 +25,7 @@ def insert_data(table_name):
         password=os.getenv('PASSWORD'),
         database="tunereviewsdb"
     )
-
+    
     cursor = conn.cursor()
 
     # Define the SQL query to insert data into the selected table
@@ -44,9 +46,9 @@ def insert_data(table_name):
     windows[table_name].destroy()
 
 # Function to create a new window for a specific table
-def create_insertion_window(table_name):
+def create_edit_window(table_name):
     new_window = tk.Toplevel(app)
-    new_window.title(f"Insert Data for {table_name}")
+    new_window.title(f"Edit Data for {table_name}")
 
     entry_fields[table_name] = []
 
@@ -61,6 +63,14 @@ def create_insertion_window(table_name):
     insert_button = tk.Button(new_window, text=f"Insert {table_name}", command=lambda name=table_name: insert_data(name))
     insert_button.pack()
 
+    # Create the "Delete" button
+    delete_button = tk.Button(new_window, text=f"Delete {table_name}", command=lambda name=table_name: delete_data(name))
+    delete_button.pack()
+
+    # Create the "Update" button
+    update_button = tk.Button(new_window, text=f"Update {table_name}", command=lambda name=table_name: update_data(name))
+    update_button.pack()
+
     # Create the "Cancel" button
     cancel_button = tk.Button(new_window, text="Cancel", command=new_window.destroy)
     cancel_button.pack()
@@ -68,9 +78,84 @@ def create_insertion_window(table_name):
     # Store the window reference
     windows[table_name] = new_window
 
+# Function to delete data
+def delete_data(table_name):
+    if table_name not in ("User", "Artist", "Album", "Song", "Review"):
+        return
+
+    values = []
+    for entry in entry_fields[table_name]:
+        value = entry.get()
+        values.append(value)
+
+    # Connect to the MySQL database
+    conn = mysql.connector.connect(
+        host=os.getenv('DATABASE_HOST'),
+        user=os.getenv('USER'),
+        password=os.getenv('PASSWORD'),
+        database="tunereviewsdb"
+    )
+    
+    cursor = conn.cursor()
+
+    # Define the SQL query to insert data into the selected table
+
+    delete_query = f"DELETE FROM {table_name} WHERE {table_name}_ID = {values[0]}"
+
+    # Execute the query with the user input
+
+    cursor.execute(delete_query)
+
+    # Commit the changes and close the database connection
+    conn.commit()
+    conn.close()
+
+    # Clear the input fields after insertion
+    for entry in entry_fields[table_name]:
+        entry.delete(0, 'end')
+
+    # Close the insertion window
+    windows[table_name].destroy()
+
+# Function to update data
+def update_data(table_name):
+    if table_name not in ("User", "Artist", "Album", "Song", "Review"):
+        return
+
+    values = []
+    for entry in entry_fields[table_name]:
+        value = entry.get()
+        values.append(value)
+
+    # Connect to the MySQL database
+    conn = mysql.connector.connect(
+        host=os.getenv('DATABASE_HOST'),
+        user=os.getenv('USER'),
+        password=os.getenv('PASSWORD'),
+        database="tunereviewsdb"
+    )
+    
+    cursor = conn.cursor()
+
+    # Define the SQL query to insert data into the selected table
+    update_query = f"UPDATE {table_name} SET {', '.join([i + '= %s' for i in tables[table_name]])} WHERE {table_name}_ID = {values[0]}"
+
+    # Execute the query with the user input
+    cursor.execute(update_query, values)
+
+    # Commit the changes and close the database connection
+    conn.commit()
+    conn.close()
+
+    # Clear the input fields after insertion
+    for entry in entry_fields[table_name]:
+        entry.delete(0, 'end')
+
+    # Close the insertion window
+    windows[table_name].destroy()
 # Create the main application window
 app = tk.Tk()
-app.title("Data Insertion")
+app.title("Tune Reviews Database GUI")
 
 # Create the entry_fields dictionary and windows dictionary
 entry_fields = {}
@@ -87,7 +172,7 @@ tables = {
 
 # Create entry fields for each table
 for table_name in tables.keys():
-    create_button = tk.Button(app, text=f"Insert Data for {table_name}", command=lambda name=table_name: create_insertion_window(name))
+    create_button = tk.Button(app, text=f"Edit Data for {table_name}", command=lambda name=table_name: create_edit_window(name))
     create_button.pack()
 
 # Start the application
